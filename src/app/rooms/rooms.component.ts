3f803eae77a802fefc56434fc9eedb3fab9c8762
LenTestProj <lennfernz2@gmail.com>
2023-11-20 05:04:56 +0530

    installed bootstrap-ngx, learnt basics of nginit and component communication basics

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -16,45 +16,47 @@ export class RoomsComponent implements OnInit {
     totalRooms: 20,
   };
 
-  roomsList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
-      price: 500,
-      photos:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating:4.5
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Room',
-      amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
-      price: 1000,
-      photos:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating:3.423
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suite',
-      amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
-      price: 1500,
-      photos:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating:2.6
-    },
-  ];
+  roomsList: RoomList[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.roomsList=[
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
+        price: 500,
+        photos:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating:4.5
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
+        price: 1000,
+        photos:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating:3.423
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, free Wi-fi, TV, bathroom, Kitchen',
+        price: 1500,
+        photos:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYkxRAHhvrOZfFUawkqWMjuYwX-qBtRXz3QWyfvcVv&s',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating:2.6
+      },
+    ]
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
