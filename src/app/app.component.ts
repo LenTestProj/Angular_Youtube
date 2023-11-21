5eb25725891b22d99d372961cc3081dd776d0b05
LenTestProj <lennfernz2@gmail.com>
2023-11-22 05:10:15 +0530

    learnt about component ref and rendering components dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ComponentRef,ElementRef,OnInit,ViewChild, ViewContainerRef } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewInit {
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  // ngAfterViewInit(): void {
+  //   const componentRef=this.vcr.createComponent(RoomsComponent)
+  //   componentRef.instance.numberOfRooms=50
+  // }
   title = 'hotelInventoryApp';
 
   role = 'User';
+
+  // @ViewChild('user',{
+  //   read:ViewContainerRef
+  // })vcr!:ViewContainerRef;
+
+  @ViewChild('name',{
+    static:true
+  }) name!:ElementRef
+
+  ngOnInit(): void {
+    this.name.nativeElement. innerText="Hello";
+  }
 }
