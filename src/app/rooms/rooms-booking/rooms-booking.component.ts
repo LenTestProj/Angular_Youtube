5bcee6d640272025134828d86ee227c9aebd5b6b
LenTestProj <lennfernz2@gmail.com>
2023-12-06 05:04:03 +0530

    learnt about dynamic routing basics

--- src/app/rooms/rooms-booking/rooms-booking.component.ts
+++ src/app/rooms/rooms-booking/rooms-booking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rooms-booking',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms-booking.component.scss']
 })
 export class RoomsBookingComponent implements OnInit {
+  id:number=0;
+  id$ !:Observable<number>;
 
-  constructor() { }
+  constructor(private router:ActivatedRoute) { }
 
   ngOnInit(): void {
+    // this.id= this.router.snapshot.params['id'];
+    // this.router.params.subscribe((params)=>{
+    //   this.id=params['id'];
+    //   console.log(params);
+    // })
+
+    // this.id$=this.router.params.pipe(
+    //   map(params=>params['id'])
+    // )
   }
 
 }
